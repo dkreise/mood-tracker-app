cccfec60e1e3ffaec69ae45873548ee752a1ae52
dkreise <dia.kreis@gmail.com>
2025-05-10 18:25:41 +0200

    added mood model

---
+++ mood-tracker/src/main/java/com/moodtracker/mood_tracker/model/Mood.java
@@ -0,0 +1,54 @@
+package com.moodtracker.mood_tracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Mood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mood;
+    private String note;
+    private LocalDate date;
+
+    public Mood() {}
+
+    public Mood(String mood, String note, LocalDate date) {
+        this.mood = mood;
+        this.note = note;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
